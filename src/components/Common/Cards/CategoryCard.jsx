2187b174803798d352517b3a469d9c237ee5b662
arshdeepsynergy <naval.sood@klearnow.com>
2024-10-18 23:36:50 +0530

    UI fix
    Signed-off-by: arshdeepsynergy <naval.sood@klearnow.com>

---
+++ src/components/Common/Cards/CategoryCard.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Box, Avatar, Typography } from '@mui/material';
+import { ArrowCircleRight, ArrowForward } from '@mui/icons-material';
+
+const CategoryCard = ({ menuCategoryImageUrl = "", name = "-", description = "" }) => {
+    return (
+        <Box
+            sx={{
+                height: 'auto',
+                position: 'relative',
+                display: 'flex',
+                justifyContent: 'center',
+            }}
+        >
+            <Box
+                position="relative"
+                height="100%"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                marginTop={"25%"}
+
+                sx={{
+                    borderRadius: '1rem',
+                    minWidth: "10rem",
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
+                    background: 'white',
+                    padding: '2rem 1rem',
+                    transition: 'all 0.3s ease',
+                    '&:hover': {
+                        boxShadow: '0px 8px 20px rgba(0, 0, 0, 0.5)',
+                        color: 'var(--primary)',
+                        transform: 'scale(1.1)',
+                        ' & .arrow-icon': {
+                            color: 'green',
+                          },
+                    },
+                }}
+            >
+
+                <Avatar
+                    src={menuCategoryImageUrl}
+                    alt="menu_image"
+                    sx={{
+                        width: 100,
+                        height: 100,
+                        position: 'absolute',
+                        top: '-50px',
+                        border: '2px solid #ccc',
+                        boxShadow: '0px 6px 16px rgba(0, 0, 0, 0.4)',
+                        transition: 'box-shadow 0.3s ease, transform 0.3s ease',
+                    }}
+                />
+
+
+                <Box sx={{ textAlign: 'center', marginTop: '2rem' }}>
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            fontWeight: '600',
+                            fontSize: '1rem',
+                        }}
+                    >
+                        {name}
+                    </Typography>
+                    <Typography variant="body1">
+                        {description ||"Explore"} <ArrowCircleRight className='arrow-icon' sx={{color:"red"}}/>
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default CategoryCard;
---
+++ src/components/Common/CarouselWrapper/data.jsx
@@ -0,0 +1,22 @@
+export const defaultResponsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+      partialVisibilityGutter: 30
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5, 
+      partialVisibilityGutter: 30
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 640 }, 
+      items: 3,
+      partialVisibilityGutter: 30
+    },
+    mobile: {
+      breakpoint: { max: 640, min: 0 }, 
+      items: 1,
+      partialVisibilityGutter: 30
+    },
+  };
