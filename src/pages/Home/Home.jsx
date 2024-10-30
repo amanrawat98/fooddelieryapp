ca1f57cfcbac0cc4f9a529807b8f75753bfddf64
arshdeepsynergy <naval.sood@klearnow.com>
2024-10-30 12:55:58 +0530

    footer fix and map dyanmicly
    Signed-off-by: arshdeepsynergy <naval.sood@klearnow.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import BannerContainer from "../../components/BannerContainer";
 import ExploreMenu from "../../components/ExploreMenu";
-import AppDownload from "../../components/AppDownload.jsx/AppDownload";
 import CategorySection from "../../components/CategorySection";
 import { useSelector } from "react-redux";
 import { Box } from "@mui/material";
@@ -17,7 +16,6 @@ const Home = () => {
         <BannerContainer bannerData={outletData?.bannerImages} />
         <ExploreMenu menuItems={filteredMenuCategories} />
         <CategorySection menuItems={filteredMenuCategories} />
-        <AppDownload />
       </Box>
     </Box>
   );
